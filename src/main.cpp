7e1a16949cc61e44839aae68f0691737f93bf893
Mykola Vankovych <mykola.vankovych1@gmail.com>
2021-04-02 10:24:10 +0200

    improving clang-format.

--- src/main.cpp
+++ src/main.cpp
@@ -24,18 +24,18 @@ static constexpr auto USAGE =
 
 int main(int argc, const char **argv)
 {
-  std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
-    { std::next(argv), std::next(argv, argc) },
-    true,// show help if requested
-    "Naval Fate 2.0");// version string
+    std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
+      { std::next(argv), std::next(argv, argc) },
+      true,// show help if requested
+      "Naval Fate 2.0");// version string
 
-  for (auto const &arg : args) {
-    std::cout << arg.first << arg.second << std::endl;
-  }
+    for (auto const &arg : args) {
+        std::cout << arg.first << arg.second << std::endl;
+    }
 
 
-  //Use the default logger (stdout, multi-threaded, colored)
-  spdlog::info("Hello, {}!", "World");
+    //Use the default logger (stdout, multi-threaded, colored)
+    spdlog::info("Hello, {}!", "World");
 
-  fmt::print("Hello, from {}\n", "{fmt}");
+    fmt::print("Hello, from {}\n", "{fmt}");
 }
